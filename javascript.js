33f994c0f4dfa51c80ff61d1fce729f92a3c0db3
janinakataja <55226673+janinakataja@users.noreply.github.com>
2020-10-22 16:57:59 +0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -6,17 +6,17 @@ var i;
 for (i = 0; i < myNodelist.length; i++) {
   var span = document.createElement("SPAN");
    var txt = document.createTextNode("✘");
-  span.className = "close";
+  span.className = "sulje";
   span.appendChild(txt);
   myNodelist[i].appendChild(span);
 }
 
 // sulje painike piilottaa listan kohteen
     
-var close = document.getElementsByClassName("close");
+var sulje = document.getElementsByClassName("sulje");
 var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
+for (i = 0; i < sulje.length; i++) {
+  sulje[i].onclick = function() {
     var div = this.parentElement;
     div.style.display = "none";
   }
@@ -35,24 +35,24 @@ list.addEventListener('click', function(ev) {
     
 function newElement() {
   var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
+  var inputValue = document.getElementById("JaninaInput").value;
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
   if (inputValue === '') {
     alert("Sinun pitää kirjoittaa jotain!");
   } else {
-    document.getElementById("myUL").appendChild(li);
+    document.getElementById("JaninaUL").appendChild(li);
   }
-  document.getElementById("myInput").value = "";
+  document.getElementById("JaninaInput").value = "";
 
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("✘");
-  span.className = "close";
+  span.className = "sulje";
   span.appendChild(txt);
   li.appendChild(span);
 
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
+  for (i = 0; i < sulje.length; i++) {
+    sulje[i].onclick = function() {
       var div = this.parentElement;
       div.style.display = "none";
     }
